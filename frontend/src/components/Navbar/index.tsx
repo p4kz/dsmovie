5373aa525686e242c50c8a9c8a2ae9ca758161f7
p4kz <wolpak@hotmail.com>
2022-03-08 00:16:57 -0300

    create Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GithubIcon} from '../../assets/img/github_logo.svg';
+import './styles.css'
+function Navbar () {
+
+    return(
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/p4kz">
+                        <div className='dsmovie-contact-container'>
+                            <GithubIcon />
+                            <p className='dsmovie-contact-link'>/p4kz</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar
