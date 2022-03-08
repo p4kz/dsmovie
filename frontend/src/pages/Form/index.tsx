35ca17b1515eb7056dea41189ff0d0d9ee38790f
p4kz <wolpak@hotmail.com>
2022-03-08 02:43:49 -0300

    create buttons navigation

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import './styles.css';
 
 function Form() {
@@ -33,10 +34,12 @@ function Form() {
                     <div className="dsmovie-form-btn-container">
                         <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
                     </div>
-                </form >
+                </form>
+                <Link to="/">
                 <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
-            </div >
-        </div >
+                </Link>                
+            </div>
+        </div>
     );
 }
 
