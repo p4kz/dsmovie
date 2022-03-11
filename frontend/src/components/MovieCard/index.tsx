a7a1eb51c377adc6b60d03917b92c16f4cbbf737
p4kz <wolpak@hotmail.com>
2022-03-11 09:56:11 -0300

    create show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -14,7 +14,7 @@ function MovieCard({movie} : Props) {
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore count={movie.count} score={movie.score}/>
 
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>    
@@ -23,4 +23,5 @@ function MovieCard({movie} : Props) {
         </div>
     );
 }
+
 export default MovieCard
